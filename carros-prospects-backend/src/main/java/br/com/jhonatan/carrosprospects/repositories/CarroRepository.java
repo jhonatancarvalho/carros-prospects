33b4c02f705561d7303699350799bfbee58a69c6
Jhonatan Carvalho <jhonatan.carvalho92@gmail.com>
2018-03-04 16:20:35 -0300

    Adicionado testes de integração

--- carros-prospects-backend/src/main/java/br/com/jhonatan/carrosprospects/repositories/CarroRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.jhonatan.carrosprospects.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import br.com.jhonatan.carrosprospects.domain.Carro;
-
-public interface CarroRepository extends JpaRepository<Carro, Integer> {
-
-}
